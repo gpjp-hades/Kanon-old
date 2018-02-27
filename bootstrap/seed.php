1aa0288b0e4896d37f79bba2bc56e787f3143fd3
Keombre <keombre8@gmail.com>
2018-02-27 02:36:18 +0100

    autoseeding from csv

--- bootstrap/seed.php
+++ bootstrap/seed.php
@@ -50,13 +50,16 @@ class seed extends \model\container {
                 "token INTEGER"
             ]);
 
-            $this->ensureTable("books", [
+            if ($this->ensureTable("books", [
                 "id INTEGER PRIMARY KEY",
                 "author TEXT",
                 "name TEXT",
-                "genere INTEGER",
+                "genere INTEGER NULL",
                 "region INTEGER"
-            ]);
+            ])) {
+                $books = $this->parseCSV('../db/kanon.csv', ['[i]id', '[i]region', '[s]author', '[s]name']);
+                $this->populate('books', $books);
+            }
 
             $this->ensureTable("users_books", [
                 "user INTEGER",
@@ -80,10 +83,30 @@ class seed extends \model\container {
     }
 
     private function populate(string $table, array $data):bool {
-        if ($this->db->insert("config", $data)) {
-            $this->logger->info("Populated table $name");
+        if ($this->db->insert($table, $data)) {
+            $this->logger->info("Populated table $table");
             return true;
         }
         return false;
+    }
+
+    private function parseCSV(string $fname, array $fields):array {
+        if (!is_file($fname))
+            throw new \InvalidArgumentException($fname . " not found");
+        
+        $handle = fopen($fname, "r");
+        $ret = [];
+        if ($handle) {
+            while (($data = fgetcsv($handle, 0, ";")) !== false) {
+                $line = [];
+                foreach ($fields as $id => $field) {
+                    $line[substr($field, 3)] = substr($field, 0, 3) == "[i]" ? (int) $data[$id] : (string) $data[$id];
+                }
+                array_push($ret, $line);
+            }
+            fclose($handle);
+        }
+        
+        return $ret;
     }
 }
